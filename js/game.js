10febb686cb427788b88808d16ba843d60980281
Johall <johnalexander.w.hall@gmail.com>
2016-01-10 00:22:49 -0500

    added minor changes to js syntax

--- js/game.js
+++ js/game.js
@@ -6,19 +6,19 @@ var gameCounter;
 alert('hi');
 //Event Listeners
 toggleButton.addEventListener('click', toggleStart)
-for(i = 0;i < gameImages.length;i++){
+for (i = 0;i < gameImages.length;i++){
   gameImages[i.addEventListener('click', gameScore)]
 }
 
 
 
-function toggleStart(){
-  if(this.getAttribute("data-state") === "start"){
+function toggleStart() {
+  if (this.getAttribute("data-state") === "start"){
     timerDisplay = 20;
     this.setAttribute('data-state','running')
     timerIntervale = setIntervale(gameRunning, 1000)
   }
-  if(this.getAttribute("data-state") === "running"){
+  if (this.getAttribute("data-state") === "running"){
     clearIntervale(timerIntervale);
     this.setAttribute('data-state','start');
     this.innerHTML = "Start";
@@ -27,11 +27,11 @@ function toggleStart(){
 function gameRunning(){
   toggleButton.innerHTML = timerDisplay;
   timerDisplay--;
-  if(timeDisplay === 0){
+  if (timeDisplay === 0){
     clearIntervale(timerIntervale);
     toggleButton.innerHtML = "Start";
     alert("Score: " + gameCounter);
-    gameCounter = 0;
+        gameCounter = 0;
 
   }
   function gameScore(){
